67aa4fcbcf6068efb0d62417178f1fd0e528ee3e
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2021-06-19 18:33:58 +0530

    typeScript

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-console.log("hello typeScript");
-//  let newArray:string[]=[];
-//  newArray=["isuru",1 ]
-var result;
-function combine(input1, input2) {
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-}
-var combineAge = combine(2, 4);
-console.log(combineAge);
-var combineName = combine('isuru', 'dilshan');
-console.log(combineName);
-function genaratedError(message, code) {
-    throw { message: message, code: code };
-}
-genaratedError("isuru", 500);
+// console.log("hel cript");
+// //  let newArray:string[]=[];
+// //  newArray=["isuru",1 ]
+// let result;
+// function combine(input1: number | string, input2: number | string) {
+//     if (typeof input1 === "number" && typeof input2 === "number") {
+//         result = input1 + input2;
+//     } else {
+//           result=input1.toString() + input2.toString();     
+//     }
+//     return result;
+// }
+// const combineAge = combine(2, 4);
+// console.log(combineAge);
+//  const combineName=combine('isuru','dilshan');
+//  console.log(combineName);
+//  function generateError(message:string , code:number){
+//      throw {message:message, errorCode:code}
+//  }
+//  generateError('isuru',500);
+var person = {
+    name: "isuru",
+    age: 20
+};
+console.log(person.name);
--- app.ts
+++ app.ts
@@ -1,33 +1,41 @@
-console.log("hello typeScript");
+// console.log("hel cript");
 
 
 
-//  let newArray:string[]=[];
+// //  let newArray:string[]=[];
 
-//  newArray=["isuru",1 ]
-let result;
-function combine(input1: number | string, input2: number | string) {
+// //  newArray=["isuru",1 ]
+// let result;
+// function combine(input1: number | string, input2: number | string) {
 
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+//     if (typeof input1 === "number" && typeof input2 === "number") {
+//         result = input1 + input2;
        
-    } else {
-          result=input1.toString() + input2.toString();     
+//     } else {
+//           result=input1.toString() + input2.toString();     
     
-    }
+//     }
 
-    return result;
+//     return result;
 
-}
+// }
 
-const combineAge = combine(2, 4);
-console.log(combineAge);
+// const combineAge = combine(2, 4);
+// console.log(combineAge);
 
- const combineName=combine('isuru','dilshan');
- console.log(combineName);
+//  const combineName=combine('isuru','dilshan');
+//  console.log(combineName);
 
- function genaratedError(message:string , code:number){
-     throw {message:message, code:code}
- }
+//  function generateError(message:string , code:number){
+//      throw {message:message, errorCode:code}
+//  }
 
- genaratedError("isuru",500);
+//  generateError('isuru',500);
+
+ const person ={
+     name:"isuru",
+     age:20
+
+ };
+console.log(person.mname);
+ 
