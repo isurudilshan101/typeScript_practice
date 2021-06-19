c207bc5fb3163363e9151b34447aeac775e31b0b
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2021-06-19 17:34:40 +0530

    redux todo app new

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log("hello typeScript");
+//  let newArray:string[]=[];
+//  newArray=["isuru",1 ]
+var result;
+function combine(input1, input2) {
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var combineAge = combine(2, 4);
+console.log(combineAge);
+var combineName = combine('isuru', 'dilshan');
+console.log(combineName);
+function genaratedError(message, code) {
+    throw { message: message, code: code };
+}
+genaratedError("isuru", 500);
--- app.ts
+++ app.ts
@@ -1,1 +1,33 @@
 console.log("hello typeScript");
+
+
+
+//  let newArray:string[]=[];
+
+//  newArray=["isuru",1 ]
+let result;
+function combine(input1: number | string, input2: number | string) {
+
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+       
+    } else {
+          result=input1.toString() + input2.toString();     
+    
+    }
+
+    return result;
+
+}
+
+const combineAge = combine(2, 4);
+console.log(combineAge);
+
+ const combineName=combine('isuru','dilshan');
+ console.log(combineName);
+
+ function genaratedError(message:string , code:number){
+     throw {message:message, code:code}
+ }
+
+ genaratedError("isuru",500);
